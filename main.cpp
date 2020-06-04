4815b7d1811c60556a6119411df08877827780f1
Felipe Viana <ninjamanco@gmail.com>
2020-06-03 21:55:11 -0300

    moving definitions to .h and creating element acess functions

--- main.cpp
+++ main.cpp
@@ -13,11 +13,15 @@ int main(){
         B.push_back(i);
     }
 
-    for(auto i=0;i<25;i++){
-        cout << A[i] << "\n";
-    }
+    A[9] = 999;
+    B[9] = 999;
 
-    B.print();
+    A.at(2) = 777;
+    B.at(2) = 777;
+
+    for(auto i=0;i<25;i++){
+        cout << A[i] << " " << B[i] << "\n";
+    }
 
     return 0;
 }
--- myVector.h
+++ myVector.h
@@ -1,5 +1,8 @@
 #include<iostream>
 
+//#################################
+//######### Declarations ##########
+//#################################
 template<typename T>
 class myVector {
 //Private elements
@@ -13,6 +16,13 @@ public:
     myVector();
     //myVector(const myVector& x);
     //myVector(int n, const T& val);
+    ~myVector();
+
+    //Element access
+    T& operator[](int idx);
+    const T& operator[](int idx) const;
+    T& at(int idx);
+    const T& at(int idx) const;
 
     //Modifiers
     void push_back(const T& val);
@@ -20,3 +30,74 @@ public:
     //DEBUG
     void print();
 };
+
+//################################
+//######### Definitions ##########
+//################################
+
+//Constructors
+template <typename T>
+myVector<T>::myVector(){
+    size = 0;
+    cap = 8;
+    elem = new T[cap];
+}
+
+template <typename T>
+myVector<T>::~myVector(){
+    delete elem;
+}
+
+/*myVector::myVector(const myVector& x){
+
+}
+
+myVector::myVector(int n, const T& val){
+
+}*/ 
+
+// Element access
+template <typename T>
+T& myVector<T>::operator[](int idx){
+    return elem[idx];
+}
+
+template <typename T>
+const T& myVector<T>::operator[](int idx) const {
+    return elem[idx];
+}
+
+template<typename T>
+T& myVector<T>::at(int idx){
+    return elem[idx];
+}
+
+template<typename T>
+const T& myVector<T>::at(int idx) const {
+    return elem[idx];
+}
+
+// Modifiers
+template <typename T>
+void myVector<T>::push_back(const T& val){
+    if(size >= cap){
+        cap *= 2;
+        T* oldElem = elem;
+        T* newElem = new T[cap];
+        for(auto i=0;i<size;i++){
+            newElem[i] = oldElem[i];
+        }
+        elem = newElem;
+        delete oldElem;
+    }
+    elem[size] = val;
+    size++;
+}
+
+// DEBUG
+template <typename T>
+void myVector<T>::print(){
+        for(auto i=0;i<size;i++){
+            std::cout << elem[i] << "\n";
+        }
+    }
